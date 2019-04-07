82f39bcd32c873cc308f1ffee4ec73170a0911dd
64BitMaster <marc.sustaita@gmail.com>
2019-04-07 00:32:38 -0500

    Update Quantization.java

--- Quantization.java
+++ Quantization.java
@@ -7,20 +7,26 @@ public class Quantization {
     
     public static void main(String[] args) throws FileNotFoundException, IOException {
     	
+    	// Create new image that reads from baboon.pgma
     	Image original = new Image("baboon");
     	
+    	// Encode original image with new max value
     	Image encodedImage12 = new Image(original, 12, true, null);
     	Image encodedImage2 = new Image(original, 2, true, null);
     	
-    	Image decodedImage12 = new Image(encodedImage12, 255, false, "baboon");
-    	Image decodedImage2 = new Image(encodedImage2, 255, false, "baboon");
+    	// Decode encoded image with original photos max value
+    	Image decodedImage12 = new Image(encodedImage12, original.maxValue, false, "baboon");
+    	Image decodedImage2 = new Image(encodedImage2, original.maxValue, false, "baboon");
     	
+    	// Create error image to show differences between original and decoded
     	Image errorImage12 = new Image(original, decodedImage12, 12);
     	Image errorImage2 = new Image(original, decodedImage2, 2);
     	
+    	// Find distortion created during encoding and decoding
     	int distortion12 = (decodedImage12.calculateDistortion(original));
     	int distortion2 = (decodedImage2.calculateDistortion(original));
     	
+    	// Save images in .pgma format
     	encodedImage12.saveFile();
     	encodedImage2.saveFile();
     	decodedImage12.saveFile();
